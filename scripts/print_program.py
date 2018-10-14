ca54dacfa7d84513d7991e359fd26143e500b40c
Alex Saalberg <asaalber@calpoly.edu>
2018-10-14 14:10:17 -0700

    Changed the wait the pid is found in print_program.py to a way that should be quicker

--- scripts/print_program.py
+++ scripts/print_program.py
@@ -23,14 +23,16 @@ def get_leaf_pid(root_pid):
 	except CalledProcessError:
 		return root_pid
 
-def get_pid_from_id(session, pane_id):
-	pane_id_list = session.cmd('list-panes', '-F', '#{pane_id}').stdout
-	pane_pid_list = session.cmd('list-panes', '-F', '#{pane_pid}').stdout
-	#pane_tty_list = session.cmd('list-panes', '-F', '#{pane_tty}').stdout
-	#child_pid_list = list(map(get_leaf_pid, pane_pid_list))
-	pane_index = pane_id_list.index(pane_id)
-	return get_leaf_pid(pane_pid_list[pane_index])
+#def get_pid_from_id(session, pane_id):
+	#pane_id_list = session.cmd('list-panes', '-F', '#{pane_id}').stdout
+	#pane_pid_list = session.cmd('list-panes', '-F', '#{pane_pid}').stdout
+	#pane_index = pane_id_list.index(pane_id)
+	#return get_leaf_pid(pane_pid_list[pane_index])
 
+
+def get_pid(pane):
+	pane_pid = session.cmd('display-message', '-p', '#{pane_pid}').stdout[0]
+	return get_leaf_pid(pane_pid)
 
 server = libtmux.Server()
 
@@ -38,13 +40,15 @@ session_num = get_current_session()
 session = server.list_sessions()[session_num]
 
 active_pane = session.attached_pane
-active_pane_id = get_id_from_pane(active_pane)
+#active_pane_id = get_id_from_pane(active_pane)
 
-active_pane_pid = get_pid_from_id(session, active_pane_id)
+active_pane_pid = get_pid(active_pane)
 active_program_name = get_program_from_pid(active_pane_pid)
 
 note_pane = active_pane.split_window(vertical=False)
-note_dir = "~/.note/"
-note_pane.send_keys("tldr "+active_program_name+" | less -r")
-note_pane.send_keys("i", enter=False)
+#note_dir = "~/.note/"
+#note_pane.send_keys("tldr "+active_program_name+" | less -r")
+note_pane.send_keys("tldr" + active_program_name)
+#note_pane.send_keys("i", enter=False)
+
 #print(active_pane_pid + ':' + active_program_name)
